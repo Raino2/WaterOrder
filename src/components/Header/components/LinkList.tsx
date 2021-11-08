1b917b842db49a1a2eb954a56af72efd51a51a2a
Raino2 <962688180@qq.com>
2021-11-08 21:17:46 +0800

    style:更新Header组件menuList子元素的宽度

--- src/components/Header/components/LinkList.tsx
+++ src/components/Header/components/LinkList.tsx
@@ -1,37 +1,37 @@
-import { Space } from "antd";
-import Link from "antd/lib/typography/Link";
-import React from "react";
-import styles from "../styles/index.module.scss";
+import { Space } from 'antd';
+import Link from 'antd/lib/typography/Link';
+import React from 'react';
+import styles from '../styles/index.module.scss';
 
 const IndexLinkList = () => {
   const linkConfig: { name: string; link: string }[] = [
     {
-      name: "首页",
-      link: "",
+      name: '首页',
+      link: '',
     },
     {
-      name: "我的订单",
-      link: "",
+      name: '我的订单',
+      link: '',
     },
     {
-      name: "给我留言",
-      link: "",
+      name: '给我留言',
+      link: '',
     },
     {
-      name: "个人中心",
-      link: "https://www.baidu.com",
+      name: '个人中心',
+      link: 'https://www.baidu.com',
     },
     {
-      name: "关于作者",
-      link: "",
+      name: '关于作者',
+      link: '',
     },
     {
-      name: "数据可视化",
-      link: "",
+      name: '数据可视化',
+      link: '',
     },
   ];
 
-  const menuWidth = 835;
+  const menuWidth = 100;
   const listLength = linkConfig.length;
   const listWidth = menuWidth / listLength;
 
@@ -43,7 +43,7 @@ const IndexLinkList = () => {
             href={item.link}
             target="_blank"
             className={styles.list}
-            style={{ width: listWidth }}
+            style={{ width: `${listWidth}%` }}
           >
             {item.name}
           </Link>
