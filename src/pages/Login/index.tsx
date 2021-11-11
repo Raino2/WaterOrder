f9a73bd2e2501f86fa61909a9efa8b649eead487
Raino1999 <zhong.weiyong@ninechapter.com>
2021-11-11 20:04:10 +0800

    style:重新排班代码格式

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,29 +1,29 @@
-import { Layout, Space } from "antd";
-import { Content, Footer, Header } from "antd/lib/layout/layout";
-import React from "react";
-import WebHeader from "../../components/Header";
-import LoginContentDescription from "./components/description";
-import styles from "./styles/index.module.scss";
+import { Layout, Space } from 'antd';
+import { Content, Footer, Header } from 'antd/lib/layout/layout';
+import React from 'react';
+import WebHeader from '../../components/Header';
+import LoginContentDescription from './components/description';
+import styles from './styles/index.module.scss';
 
 const LoginPage = () => {
   return (
     <Layout>
       <Header
         style={{
-          backgroundColor: "white",
-          boxShadow: "0px 6px 16px rgba(0,0,0,0.08) ",
-          padding: "0 20px",
+          backgroundColor: 'white',
+          boxShadow: '0px 6px 16px rgba(0,0,0,0.08) ',
+          padding: '0 20px',
         }}
       >
         <WebHeader />
       </Header>
-      <Content style={{ minHeight: 800, position: "relative" }}>
+      <Content style={{ minHeight: 800, position: 'relative' }}>
         <div className={styles.desArea}>
           <LoginContentDescription />
         </div>
       </Content>
-      <Footer style={{ display: "flex", justifyContent: "center" }}>
-        <Space size={50} style={{ color: "rgba(0,0,0,0.45)" }}>
+      <Footer style={{ display: 'flex', justifyContent: 'center' }}>
+        <Space size={50} style={{ color: 'rgba(0,0,0,0.45)' }}>
           <div>
             CopyRight© <a href="https://github.com/Raino2">Best_Raino</a>
           </div>
