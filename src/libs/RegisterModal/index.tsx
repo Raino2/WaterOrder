0660cffa7e9df237b2a9da540880c5b3f85bb721
Raino1999 <zhong.weiyong@ninechapter.com>
2021-11-16 19:36:56 +0800

    feat:增加注册功能

--- src/libs/RegisterModal/index.tsx
+++ src/libs/RegisterModal/index.tsx
@@ -1,5 +1,11 @@
-import { Button, Form, Input, Modal } from 'antd';
+import { Alert, Button, Form, Input, Modal, Space } from 'antd';
 import React from 'react';
+import { SmileOutlined } from '@ant-design/icons';
+import MessageFilled from '@ant-design/icons/lib/icons/MessageFilled';
+import LockFilled from '@ant-design/icons/lib/icons/LockFilled';
+import { placeholder } from '@babel/types';
+import PhoneFilled from '@ant-design/icons/lib/icons/PhoneFilled';
+import MailFilled from '@ant-design/icons/lib/icons/MailFilled';
 
 type TProps = {
   visible: boolean;
@@ -13,14 +19,27 @@ const RegisterModal = (props: TProps) => {
 
   const modalTitle = () => {
     return (
-      <h2
-        style={{
-          textAlign: 'center',
-          fontWeight: 800,
-        }}
-      >
-        新用户注册
-      </h2>
+      <>
+        <h1
+          style={{
+            textAlign: 'center',
+            fontWeight: 800,
+            height: 70,
+            lineHeight: '70px',
+          }}
+        >
+          <MessageFilled style={{ color: 'rgb(86,201,245)', marginRight: 10 }} />
+          新用户注册
+        </h1>
+        <div
+          style={{
+            color: 'rgba(0,0,0,0.35)',
+            textAlign: 'center',
+          }}
+        >
+          超级好用的在线订水网站 OWO
+        </div>
+      </>
     );
   };
 
@@ -33,23 +52,38 @@ const RegisterModal = (props: TProps) => {
       centered
       destroyOnClose
     >
-      <Form name="register" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} autoComplete="off">
-        <Form.Item name="userName" label="用户名">
-          <Input />
+      <Form
+        name="register"
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 16, offset: 4 }}
+        autoComplete="off"
+        layout="horizontal"
+        size="large"
+      >
+        <Form.Item name="userName">
+          <Input prefix={<SmileOutlined />} placeholder="请输入账号" />
         </Form.Item>
-        <Form.Item name="userPass" label="密  码">
-          <Input.Password />
+        <Form.Item name="userPass">
+          <Input.Password prefix={<LockFilled />} placeholder="请输入密码" />
         </Form.Item>
-        <Form.Item name="phone" label="手  机">
-          <Input />
+        <Form.Item name="phone">
+          <Input prefix={<PhoneFilled />} placeholder="请输入手机号" />
         </Form.Item>
-        <Form.Item name="email" label="邮  箱">
-          <Input />
+        <Form.Item name="email">
+          <Input prefix={<MailFilled />} placeholder="请输入邮箱号" />
         </Form.Item>
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit" block>
-            注册账号
-          </Button>
+        <Form.Item>
+          <Alert message="注册成功后,请去'个人中心'完善信息" type="warning" />
+        </Form.Item>
+        <Form.Item wrapperCol={{ span: 16, offset: 6 }}>
+          <Space size="large" align="center">
+            <Button type="primary" htmlType="submit">
+              注册账号
+            </Button>
+            <Button type="dashed" htmlType="button">
+              我再逛逛
+            </Button>
+          </Space>
         </Form.Item>
       </Form>
     </Modal>
